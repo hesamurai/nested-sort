4bbf6c84366e351bb0e4245a0cfed27c5f1f527a
Hesam Bahrami (Genzo) <genzographics@gmail.com>
2020-05-24 13:48:38 +0100

    Extending the render method to render nested lists. Also, adding some tests.

--- src/data-engine.js
+++ src/data-engine.js
@@ -5,17 +5,133 @@ class DataEngine {
    */
   constructor({ data }) {
     this.data = data
+    this.sortedData = []
+    this.sortedDataDomArray = []
   }
 
-  getListItemsDom() {
-    return this.data.map(item => {
-      const listItem = document.createElement('li')
-      listItem.dataset.id = item.id
-      listItem.innerHTML = item.text
-      return listItem
+  /**
+   * @returns {array}
+   */
+  sortListItems() {
+    this.sortedData = [...this.data].sort((item1, item2) => {
+      if (!item1.parent && item2.parent) return -1
+      return (!item2.parent && item1.parent) ? 1 : 0;
+    });
+
+    return this.sortedData
+  }
+
+  /**
+   * @param {object} item
+   * @param {string} nodeName
+   * @returns {HTMLElement}
+   */
+  createItemElement(item, nodeName = 'li') {
+    const { id, text } = item
+    const el = document.createElement(nodeName)
+    el.dataset.id = id
+    if (nodeName === 'li') el.innerHTML = text
+
+    return el
+  }
+
+  /**
+   * @param {HTMLElement} node
+   * @param {object} item
+   * @returns {boolean}
+   */
+  elementIsParentOfItem(node, item) {
+    return node.dataset.id === `${item.parent}`
+  }
+
+  /**
+   * @param {HTMLElement} node
+   * @param {object} item
+   * @param {string} nodeName
+   * @returns {HTMLElement}
+   */
+  getParentNodeOfItem(node, item, nodeName) {
+    return node.querySelector(`${nodeName}[data-id="${item.parent}"]`)
+  }
+
+  /**
+   * @param {HTMLElement} node
+   * @param {object} item
+   * @returns {boolean}
+   */
+  elementIsAncestorOfItem(node, item) {
+    return !!this.getParentNodeOfItem(node, item, 'li')
+  }
+
+  /**
+   * @param {HTMLElement} node
+   * @param {object} item
+   * @returns {HTMLElement}
+   */
+  getDirectListParentOfItem(node, item) {
+    return this.getParentNodeOfItem(node, item, 'ul')
+  }
+
+  /**
+   * @param {object} item
+   * @returns {boolean}
+   */
+  maybeAppendItemToParentDom(item) {
+    const { parent } = item
+    const topParent = this.sortedDataDomArray.find(topLevelListItem => {
+      return this.elementIsParentOfItem(topLevelListItem, item) || this.elementIsAncestorOfItem(topLevelListItem, item)
     })
+
+    if (!topParent) return false;
+
+    const listItem = this.createItemElement(item)
+    let directParentList = this.getDirectListParentOfItem(topParent, item)
+
+    if (!directParentList) {
+      // so we need to create the direct parent UL and append it to the direct parent LI
+      directParentList = this.createItemElement({ id: parent }, 'ul')
+      const directParentListItem = this.getParentNodeOfItem(topParent, item, 'li') || topParent
+      directParentListItem.appendChild(directParentList)
+    }
+
+    directParentList.appendChild(listItem)
+
+    return true
   }
 
+  /**
+   * @returns {array}
+   */
+  getListItemsDom() {
+    this.sortedDataDomArray = []
+    let processedItems = []
+
+    while (processedItems.length !== this.sortListItems().length) {
+      processedItems = this.sortedData.reduce((processedItems, item) => {
+        const { id } = item
+        if (processedItems.includes(id)) return processedItems
+
+        let itemAdded
+        if (!item.parent) {
+          const listItem = this.createItemElement(item)
+          this.sortedDataDomArray.push(listItem)
+          itemAdded = true
+        } else {
+          itemAdded = this.maybeAppendItemToParentDom(item)
+        }
+
+        if (itemAdded) processedItems.push(id)
+
+        return processedItems
+      }, processedItems)
+    }
+
+    return this.sortedDataDomArray
+  }
+
+  /**
+   * @returns {HTMLUListElement}
+   */
   render() {
     const list = document.createElement('ul')
     this.getListItemsDom().forEach(listItem => list.appendChild(listItem))
--- src/data-engine.test.js
+++ src/data-engine.test.js
@@ -13,6 +13,66 @@ describe('DataEngine class', () => {
       }
 
       expect((new DataEngine(dataEngineConfig)).data).toEqual(data)
+      expect((new DataEngine(dataEngineConfig)).sortedData).toEqual([])
+      expect((new DataEngine(dataEngineConfig)).sortedDataDomArray).toEqual([])
+    })
+  })
+
+  describe('sortListItems method', () => {
+    it('should gather all the items without any parent at the beginning of the array', () => {
+      const data = [
+        { id: 4, parent: 3 },
+        { id: 1 },
+        { id: 2, parent: 6 },
+        { id: 3 },
+        { id: 5, parent: null },
+        { id: 6, parent: '' },
+      ]
+      const dataEngineConfig = {
+        data
+      }
+
+      expect((new DataEngine(dataEngineConfig)).sortListItems()).toEqual([
+        { id: 1 },
+        { id: 3 },
+        { id: 5, parent: null },
+        { id: 6, parent: '' },
+        { id: 4, parent: 3 },
+        { id: 2, parent: 6 },
+      ])
+    })
+  })
+
+  describe('createItemElement method', () => {
+    describe('when no nodeName argument is passed', () => {
+      it('should return an LI node', () => {
+        const item = { id: 1, text: 'Hi' }
+        const engine = new DataEngine({})
+        expect(engine.createItemElement(item).nodeName).toBe('LI')
+      })
+
+      it('should set the innerHtml correctly', () => {
+        const text = 'Hello world'
+        const item = { id: 1, text }
+        const engine = new DataEngine({})
+        expect(engine.createItemElement(item).innerHTML).toBe(text)
+      })
+    })
+
+    describe('when `ul` ias passed as the nodeName argument', () => {
+      it('should NOT set the innerHtml', () => {
+        const text = 'Hello world'
+        const item = { id: 1, text }
+        const engine = new DataEngine({})
+        expect(engine.createItemElement(item, 'ul').innerHTML).toBe('')
+      })
+    })
+
+    it('should set the data-id attribute with the correct value', () => {
+      const id = 1
+      const item = { id, text: 'Hi' }
+      const engine = new DataEngine({})
+      expect(engine.createItemElement(item).dataset.id).toBe(`${id}`)
     })
   })
 
@@ -34,6 +94,71 @@ describe('DataEngine class', () => {
         '<li data-id="3">Three</li>'
 
       expect((new DataEngine(dataEngineConfig)).render()).toEqual(list)
+    })
+
+    it('should return a nested list when data items have parent property and have a child-parent structure', () => {
+      const data = [
+        { id: 1, text: 'One' },
+        { id: 11, text: 'One-One', parent: 1 },
+        { id: 2, text: 'Two' },
+        { id: 3, text: 'Three' },
+        { id: 1121, text: 'One-One-Two-One', parent: 112 },
+        { id: 1122, text: 'One-One-Two-Two', parent: 112 },
+        { id: 1123, text: 'One-One-Two-Three', parent: 112 },
+        { id: 4, text: 'Four' },
+        { id: 41, text: 'Four-One', parent: 4 },
+        { id: 42, text: 'Four-Two', parent: 4 },
+        { id: 421, text: 'Four-Two-One', parent: 42 },
+        { id: 422, text: 'Four-Two-Two', parent: 42 },
+        { id: 423, text: 'Four-Two-Three', parent: 42 },
+        { id: 5, text: 'Five' },
+        { id: 12, text: 'One-Two', parent: 1 },
+        { id: 111, text: 'One-One-One', parent: 11 },
+        { id: 112, text: 'One-One-Two', parent: 11 },
+        { id: 113, text: 'One-One-Three', parent: 11 },
+      ]
+      const dataEngineConfig = {
+        data
+      }
+
+      const list = document.createElement('ul')
+      list.innerHTML =
+        '<li data-id="1">One' +
+          '<ul data-id="1">' +
+            '<li data-id="11">One-One' +
+              '<ul data-id="11">' +
+                '<li data-id="111">One-One-One</li>' +
+                '<li data-id="112">One-One-Two' +
+                  '<ul data-id="112">' +
+                    '<li data-id="1121">One-One-Two-One</li>' +
+                    '<li data-id="1122">One-One-Two-Two</li>' +
+                    '<li data-id="1123">One-One-Two-Three</li>' +
+                  '</ul>' +
+                '</li>' +
+                '<li data-id="113">One-One-Three</li>' +
+              '</ul>' +
+            '</li>' +
+            '<li data-id="12">One-Two</li>' +
+          '</ul>' +
+        '</li>' +
+        '<li data-id="2">Two</li>' +
+        '<li data-id="3">Three</li>' +
+        '<li data-id="4">Four' +
+          '<ul data-id="4">' +
+            '<li data-id="41">Four-One</li>' +
+            '<li data-id="42">Four-Two' +
+              '<ul data-id="42">' +
+                '<li data-id="421">Four-Two-One</li>' +
+                '<li data-id="422">Four-Two-Two</li>' +
+                '<li data-id="423">Four-Two-Three</li>' +
+              '</ul>' +
+            '</li>' +
+          '</ul>' +
+        '</li>' +
+        '<li data-id="5">Five</li>'
+
+      expect((new DataEngine(dataEngineConfig)).render()).toEqual(list)
+      expect(list.querySelectorAll('li').length).toEqual(data.length)
     })
   })
 })
