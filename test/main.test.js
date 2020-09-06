137ab8fd1c05aafc25c541701d062c857f65ab1c
Hesam Bahrami <hb@gilgaz.com>
2020-09-06 15:36:21 +0100

    Adding some tests for the dragenter event.

--- test/main.test.js
+++ test/main.test.js
@@ -173,5 +173,115 @@ describe('NestedSort', () => {
         expect(setData).toHaveBeenCalledTimes(1)
       })
     })
+
+    describe('dragenter event', () => {
+      describe('when event target is either an LI or a UL element', () => {
+        it('should prevent the default behaviour to allow dropping', () => {
+          const ns = new NestedSort({
+            data: [
+              { id: 1, text: 'One' },
+              { id: 4, text: 'One-One', parent: 1 },
+              { id: 2, text: 'Two' },
+              { id: 3, text: 'Three' },
+            ],
+            el: `#${dynamicListWrapperId}`,
+          })
+
+          // let's pretend we're dragging the item Three
+          const item = document.querySelector('[data-id="3"]')
+          const setData = jest.fn()
+          item.dispatchEvent(
+            createEvent('dragstart', {
+              dataTransfer: { setData }
+            })
+          )
+
+          const items = [
+            document.querySelector('li[data-id="1"]'),
+            document.querySelector('ul[data-id="1"]'),
+          ]
+
+          items.forEach(item => {
+            const preventDefault = jest.fn()
+            item.dispatchEvent(
+              createEvent('dragenter', {
+                preventDefault,
+              })
+            )
+
+            expect(preventDefault).toHaveBeenCalledTimes(1)
+          })
+        })
+
+        it('should remove the ns-targeted class name from the previous targeted item and add it to the newly targeted one', () => {
+          const ns = new NestedSort({
+            data: [
+              { id: 1, text: 'One' },
+              { id: 4, text: 'One-One', parent: 1 },
+              { id: 2, text: 'Two' },
+              { id: 3, text: 'Three' },
+            ],
+            el: `#${dynamicListWrapperId}`,
+          })
+
+          // let's pretend we're dragging the item Three
+          const item = document.querySelector('[data-id="3"]')
+          const setData = jest.fn()
+          item.dispatchEvent(
+            createEvent('dragstart', {
+              dataTransfer: { setData }
+            })
+          )
+
+          const item1 = document.querySelector('li[data-id="1"]')
+          const item2 = document.querySelector('ul[data-id="1"]')
+
+          const dragEnterEvent = createEvent('dragenter', {
+            preventDefault: jest.fn(),
+          })
+
+          item1.dispatchEvent(dragEnterEvent)
+          expect(item1.classList).toContain('ns-targeted')
+
+          item2.dispatchEvent(dragEnterEvent)
+          expect(item1.classList).not.toContain('ns-targeted')
+          expect(item2.classList).toContain('ns-targeted')
+        })
+
+        it('should set this.targetedNode to the event target element', () => {
+          const ns = new NestedSort({
+            data: [
+              { id: 1, text: 'One' },
+              { id: 4, text: 'One-One', parent: 1 },
+              { id: 2, text: 'Two' },
+              { id: 3, text: 'Three' },
+            ],
+            el: `#${dynamicListWrapperId}`,
+          })
+
+          // let's pretend we're dragging the item Three
+          const item = document.querySelector('[data-id="3"]')
+          const setData = jest.fn()
+          item.dispatchEvent(
+            createEvent('dragstart', {
+              dataTransfer: { setData }
+            })
+          )
+
+          const item1 = document.querySelector('li[data-id="1"]')
+          const item2 = document.querySelector('ul[data-id="1"]')
+
+          const dragEnterEvent = createEvent('dragenter', {
+            preventDefault: jest.fn(),
+          })
+
+          item1.dispatchEvent(dragEnterEvent)
+          expect(ns.targetedNode).toEqual(item1)
+
+          item2.dispatchEvent(dragEnterEvent)
+          expect(ns.targetedNode).toEqual(item2)
+        })
+      })
+    })
   })
 })
