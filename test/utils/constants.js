fce87739e4b49042d6ee5ccdafbb9f69fd0ac648
Hesam Bahrami <hb@gilgaz.com>
2021-01-01 12:19:04 +0000

    refactor the server rendered list related code in main.test.js

---
+++ test/utils/constants.js
@@ -0,0 +1,1 @@
+export const STATIC_LIST_WRAPPER_ID = 'static-list-wrapper-id'
--- test/utils/dom.js
+++ test/utils/dom.js
@@ -1,3 +1,7 @@
+import {
+  STATIC_LIST_WRAPPER_ID
+} from './constants'
+
 export const createEvent = (type, props = {}) => {
   const event = new Event(type, { bubbles: props.bubbles || true })
   delete props.bubbles // this property cannot be set on the event object
@@ -14,4 +18,16 @@ export const createEvent = (type, props = {}) => {
   )
 
   return event
+}
+
+export const initServerRenderedList = () => {
+  document.body.innerHTML = `
+    <div>
+      <ul id="${STATIC_LIST_WRAPPER_ID}">
+        <li data-id="1">One</li>
+        <li data-id="2">Two</li>
+        <li data-id="3">Three</li>
+      </ul>
+    </div>
+  `
 }
